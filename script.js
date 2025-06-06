b44999272d05a990d6745906e802b32bf0e4a4c6
Ivanarre <arre2397@gmail.com>
2025-06-06 10:38:05 +0800

    added-player-logic

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 'use strict';
 
 // Selected elements
-const score0 = document.getElementById('score--0');
-const currentScore0 = document.getElementById('current--0');
-const score1 = document.getElementById('score--1');
-const currentScore1 = document.getElementById('score--1');
+const player0Ele = document.querySelector('.player--0');
+const player1Ele = document.querySelector('.player--1');
+const score0Ele = document.getElementById('score--0');
+const currentScore0Ele = document.getElementById('current--0');
+const score1Ele = document.getElementById('score--1');
+const currentScore1Ele = document.getElementById('score--1');
 const diceEle = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn-new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0.textContent = 0;
-score1.textContent = 0;
+// // Starting conditions
+score0Ele.textContent = 0;
+score1Ele.textContent = 0;
 diceEle.classList.add('hidden');
 
+const scores = [0, 0];
+let activePlayer = 0;
 let currentScore = 0;
 
-// Rolling dice functionality
+// // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   const randomDice = Math.trunc(Math.random() * 6) + 1;
 
@@ -26,7 +30,14 @@ btnRoll.addEventListener('click', function () {
 
   if (randomDice !== 1) {
     currentScore += randomDice;
-    currentScore0.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Ele.classList.toggle('player--active');
+    player1Ele.classList.toggle('player--active');
   }
 });
